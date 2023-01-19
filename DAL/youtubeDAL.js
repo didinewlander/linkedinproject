96df42c04f40494a90c986f811ecc8766530c052
newlander <ydidya.n@gmail.com>
2023-01-19 18:30:11 +0200

    fix jsonFile error

--- DAL/youtubeDAL.js
+++ DAL/youtubeDAL.js
@@ -1,11 +1,11 @@
 const axios = require('axios');
-const fs = require('jsonFile');
+//const fs = require('jsonfile');
 const file = '../Static/json/data.json';
 const getAllVideos = () => {
     console.log("retrived api");
     return axios.get("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCPVvBjoprUsc008y9zGiBKw&maxResults=100&key=AIzaSyDPfRDUAbOkhJRcVsRABhppv9MSUhkfW0Q");
 };
-const getJsonVideos = () => {
-    return fs.readFile(file);
-}
+// const getJsonVideos = () => {
+//     return fs.readFile(file);
+// }
 module.exports = { getAllVideos, getJsonVideos };
