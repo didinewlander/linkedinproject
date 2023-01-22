8d26c13012907a1b9fb7e88c2f5c22aaacb062ed
newlander <ydidya.n@gmail.com>
2023-01-22 12:03:55 +0200

    no branch! be careful

--- index.js
+++ index.js
@@ -1,38 +1,104 @@
+/*------ REQUIREMENTS ------*/
+
 const express = require('express');
 const cors = require('cors');
-const app = express();
 const path = require('path');
-require('dotenv').config();
+const bcrypt = require('bcrypt') // for encrypting
+const passport = require('passport') // for authenticating
+const flash = require('express-flash') // for alerting in server
+const session = require('express-session') // for session
+const methodOverride = require('method-override') // for overriding post method from login
+const initializePassport = require('./passport-config') // for passport configuration
+
+/*------ SECRET SETUP ------*/
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+const userUri = process.env.USER_DB_CONNECTION;
+const classUri = process.env.CLASS_DB_CONNECTION;
+const sikumUri = process.env.SIKUM_DB_CONNECTION;
+const youtubeAPI = process.env.YOUTUBE_KARNASH_API;
+
+/*------ AUTHENTICATION SETUP ------*/
+initializePassport(passport,
+    email => { return users.find(user => user.email === email) },
+    id => { return users.find(user => user.id === id) });
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+
+    res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/')
+    }
+    next()
+}
+/*------ DATABASE SETUP ------*/
 
 const class_connection = require('./Configs/classDB');
 const sikumim_connection = require('./Configs/sikumDB');
 const users_connection = require('./Configs/userDB');
 
+
+/*------ ROUTES SETUP ------*/
+
 const userRouter = require('./Routers/userRouter');
 const classRouter = require('./Routers/classRouter');
 const sikumRouter = require('./Routers/sikumRouter');
 const youtubeAPIRouter = require('./Routers/youtubeRouter');
-const userUri = process.env.USER_DB_CONNECTION;
-const classUri = process.env.CLASS_DB_CONNECTION;
-const sikumUri = process.env.SIKUM_DB_CONNECTION;
-const youtubeAPI = process.env.YOUTUBE_KARNASH_API;
-const port = 3000;
+
+/*------ EXPRESS APP SETUP ------*/
+
+const app = express();
+
+app.set('view-engine', 'ejs')
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/', express.static(path.join(__dirname, '/Static')))
-/*----------------Routes in the website--------------------*/
+app.use(express.urlencoded({ extended: false }))
+app.use(flash());
+app.use(session({
+    secret: process.env.ACCESS_TOKEN_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride('_method'));
+app.use('/', express.static(path.join(__dirname, '/public')))
 
-app.use('/', require('./Routers/rootRouter'));
-app.use('/Dashboard', require('./Routers/dashboardRouter'));
+/*------ WEBSITE SETUP ------*/
 
-app.use('/json', youtubeAPIRouter);
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', { name: req.user.name });
+});
+
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs')
+})
+
+app.post('login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureMessage: true,
+    failureMessage: "Username Don't exist"
+}))
+// app.use('/', require('./Routers/rootRouter'));
+// app.use('/Dashboard', require('./Routers/dashboardRouter'));
+
+// app.use('/json', youtubeAPIRouter);
 
 app.all('*', (req, res) => {
     res.status(404)
     if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'Static/html', '404.html'))
+        res.sendFile(path.join(__dirname, 'public/views', '404.html'))
     } else if (req.accepts('json')) {
         res.json({ message: '404 Not Found' })
     } else {
@@ -62,4 +128,4 @@ app.all('*', (req, res) => {
 //     scheduleRouter;
 // });
 
-app.listen(port, () => console.log('listening on port ' + port));
+app.listen(3000);
