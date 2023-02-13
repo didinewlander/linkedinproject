d9bdf9c24a0cef210e33f312d890c608fc046b45
newlander <ydidya.n@gmail.com>
2023-02-13 16:37:02 +0200

    completed basic calendar design - required to turn into responsive and dynamic, and compatible to mobile devices

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -85,3 +85,419 @@ fetch("/jsontest")
             return { name: result.name, email: result.email, element: resultView };
         })
     });
+
+
+function loadCalendarDay(dayIndex) {
+
+    const data = [
+        {
+            day: 1,
+            parts:
+                [[
+                    {
+                        topic: 'ליקוטי מוהר"ן',
+                        schoolGrade: "שיעור ט' ומעלה",
+                        name: "הרב אלעזר הבר",
+                        hour: "8:45"
+                    },
+                    {
+                        topic: "שיעור בוקר + פתיחה לסדר",
+                        schoolGrade: "שיעור א'",
+                        name: "ר''מ הכיתה",
+                        hour: "9:30"
+                    },
+                    {
+                        topic: "שיעור עיון",
+                        schoolGrade: "לפי כיתות",
+                        name: "כל הישיבה",
+                        hour: "12:00"
+                    }
+                ],
+                [
+                    {
+                        topic: "אורות הקודש",
+                        schoolGrade: "שיעור ה'",
+                        name: "הרב אלי גרין",
+                        hour: "14:15"
+                    },
+                    {
+                        topic: "סוגיות בעיון",
+                        schoolGrade: "כל הישיבה",
+                        name: "הרב אברהם קורצוייל",
+                        hour: "17:45"
+                    },
+
+                    {
+                        topic: "חבורות כיתתיות",
+                        schoolGrade: "שיעור ב'",
+                        name: "הרב מדן דריהם / הרב מוטי שראל",
+                        hour: "17:45"
+                    }
+                ],
+                [
+                    {
+                        topic: "כוזרי",
+                        schoolGrade: "שיעור א'",
+                        name: "רבני שיעור א'",
+                        hour: "18:30"
+                    },]
+                ]
+        },
+
+        {
+            day: 2,
+            parts: [
+                [
+                    {
+                        topic: "שיעור בוקר + פתיחה לסדר",
+                        schoolGrade: "שיעור א'",
+                        name: "ר''מ הכיתה",
+                        hour: "9:30"
+                    },
+                    {
+                        topic: "שיעור עיון",
+                        schoolGrade: "לפי כיתות",
+                        name: "כל הישיבה",
+                        hour: "12:00"
+                    }
+                ],
+                [
+                    {
+                        topic: "עין איה",
+                        schoolGrade: "שיעור ו'",
+                        name: "הרב אלי גרין",
+                        hour: "14:15"
+                    },
+                    {
+                        topic: "סוגיות בעיון",
+                        schoolGrade: "כל הישיבה",
+                        name: "הרב אברהם קורצוייל",
+                        hour: "17:45"
+                    },
+
+                ],
+                [
+                    {
+                        topic: "שמונה פרקים לרמבם",
+                        schoolGrade: "שיעור א'",
+                        name: "הרב שי גרין",
+                        hour: "18:30"
+                    },
+                    {
+                        topic: "תחיית עם-ישראל",
+                        schoolGrade: "שיעור ב'",
+                        name: "הרב אבי חורי",
+                        hour: "18:30"
+                    },
+                    {
+                        topic: "אורות התשובה",
+                        schoolGrade: "שיעור ד'",
+                        name: "הרב ציון כהן",
+                        hour: "18:30"
+                    },
+                    {
+                        topic: "תניא",
+                        schoolGrade: "שיעור ה'",
+                        name: "הרב שי גרין",
+                        hour: "18:30"
+                    }
+
+                ]
+            ]
+        },
+        {
+            day: 3,
+            parts:
+                [[
+                    {
+                        topic: "שיעור בוקר + פתיחה לסדר",
+                        schoolGrade: "שיעור א'",
+                        name: "ר''מ הכיתה",
+                        hour: "9:30"
+                    }
+                    ,
+                    {
+                        topic: "שיעור עיון + קניין תורה",
+                        schoolGrade: "שיעור א'",
+                        name: "ר''מ הכיתה",
+                        hour: "11:30"
+                    },
+                    {
+                        topic: "שיעור עיון",
+                        schoolGrade: "לפי כיתות",
+                        name: "כל הישיבה",
+                        hour: "12:00"
+                    }
+                ],
+                [
+                    {
+                        topic: "דעת תבונות",
+                        schoolGrade: "שיעור ח' ומעלה",
+                        name: "הרב רפאל טסמה",
+                        hour: "14:15"
+                    },
+                    {
+                        topic: "אורות התורה",
+                        schoolGrade: "שיעור ב'",
+                        name: "הרב מיכאל רוטנברג",
+                        hour: "17:45"
+                    },
+                    {
+                        topic: "נתיב הזוגיות",
+                        schoolGrade: "שיעור ד'",
+                        name: "הרב אבי מטלון",
+                        hour: "17:45"
+                    },
+                    {
+                        topic: "אורות התחיה",
+                        schoolGrade: "שיעור ה'",
+                        name: "הרב שי גרין",
+                        hour: "17:45"
+                    },
+                    {
+                        topic: "דעת תבונות",
+                        schoolGrade: "שיעור ו'",
+                        name: "הרב רפאל טסמה",
+                        hour: "17:45"
+                    },
+                    {
+                        topic: "סוגיות באמונה",
+                        schoolGrade: "כל הישיבה",
+                        name: "הרב אברהם קורצוייל",
+                        hour: "17:45"
+                    }
+                ],
+                [
+                    {
+                        topic: "שיעור כללי",
+                        schoolGrade: "כל הישיבה",
+                        name: "ראשי הישיבה",
+                        hour: "18:30"
+                    },
+                    {
+                        topic: "תורת חיים",
+                        schoolGrade: "שיעור א'",
+                        name: "הרב משה רוזנבאום",
+                        hour: "18:30"
+                    }
+                ]
+                ]
+        },
+        {
+            day: 4,
+            parts: [
+                [
+                    {
+                        topic: "ישראל ותחיתו",
+                        schoolGrade: "שיעור ה' ומעלה",
+                        name: "הרב ציון כהן",
+                        hour: "8:45"
+                    },
+                    {
+                        topic: "שיעור בוקר + פתיחה לסדר",
+                        schoolGrade: "שיעור א'",
+                        name: "ר''מ הכיתה",
+                        hour: "9:30"
+                    },
+                    {
+                        topic: "שיעור עיון",
+                        schoolGrade: "לפי כיתות",
+                        name: "כל הישיבה",
+                        hour: "12:00"
+                    }
+                ],
+                [
+                    {
+                        topic: "תפילה",
+                        schoolGrade: "שיעור ב'",
+                        name: "הרב שי גרין",
+                        hour: "15:30"
+                    },
+                    {
+                        topic: "סוגיות בעיון",
+                        schoolGrade: "כל הישיבה",
+                        name: "הרב אברהם קורצוייל",
+                        hour: "17:30"
+                    }
+                ],
+                [
+                    {
+                        topic: "מסילה לעובד ה'",
+                        schoolGrade: "שיעור א'",
+                        name: "הרב דוד מור יוסף",
+                        hour: "18:30"
+                    },
+                    {
+                        topic: "מסע אל מידות הנפש",
+                        schoolGrade: "שיעור ב'",
+                        name: "הרב אבי חורי",
+                        hour: "18:30"
+                    },
+                    {
+                        topic: "אורות התחיה",
+                        schoolGrade: "שיעור ד'",
+                        name: "הרב מיכאל רוטנברג",
+                        hour: "18:30"
+                    },
+                    {
+                        topic: "נושאים באמונה",
+                        schoolGrade: "שיעור ה'",
+                        name: "הרב רפאל טסמה",
+                        hour: "18:30"
+                    },
+                    {
+                        topic: "אברך במבה",
+                        schoolGrade: "שיעור א'",
+                        name: "אברכי הישיבה",
+                        hour: "22:00"
+                    }
+                ]
+            ]
+        },
+        {
+            day: 5,
+            parts: [
+                [
+                    {
+                        topic: "נצח ישראל",
+                        schoolGrade: "שיעור ה'",
+                        name: "הרב אלעזר הבר",
+                        hour: "8:45"
+                    },
+                    {
+                        topic: "אורות הקודש",
+                        schoolGrade: "כולל",
+                        name: "הרב ציון כהן",
+                        hour: "8:45"
+                    },
+                    {
+                        topic: "שיעור בוקר + פתיחה לסדר",
+                        schoolGrade: "שיעור א'",
+                        name: "ר''מ הכיתה",
+                        hour: "9:30"
+                    },
+                    {
+                        topic: "שיעור אמונה",
+                        schoolGrade: "שיעור ד-ה",
+                        name: "ר''מי הכיתות",
+                        hour: "12:00"
+                    },
+                    {
+                        topic: "שיעור עיון",
+                        schoolGrade: "לפי כיתות",
+                        name: "כל הישיבה",
+                        hour: "12:00"
+                    }
+                ],
+                [
+                    {
+                        topic: "עין איה",
+                        schoolGrade: "שיעור ז' ומעלה",
+                        name: "הרב אלי גרין",
+                        hour: "14:15"
+                    },
+                    {
+                        topic: "תפילה",
+                        schoolGrade: "שיעור ב'",
+                        name: "הרב שי גרין",
+                        hour: "15:30"
+                    },
+                    {
+                        topic: "סוגיות בעיון",
+                        schoolGrade: "כל הישיבה",
+                        name: "הרב אברהם קורצוייל",
+                        hour: "17:30"
+                    }
+                ],
+                [
+                    {
+                        topic: "הכנה ואהבה ללימוד התורה",
+                        schoolGrade: "שיעור א'",
+                        name: "הרב דוד מור יוסף",
+                        hour: "18:30"
+                    },
+                    {
+                        topic: "כוזרי",
+                        schoolGrade: "שיעור ב'",
+                        name: "חבורות",
+                        hour: "18:30"
+                    },
+                    {
+                        topic: "אקטואליה בפרשת השבוע",
+                        schoolGrade: "כל הישיבה",
+                        name: "הרב משה רוזנבאום",
+                        hour: "22:00"
+                    }
+                ]
+            ]
+        },
+        {
+            day: 6,
+            parts:
+                [[
+                    {
+                        topic: "לימוד עצמי",
+                        schoolGrade: "",
+                        name: " ",
+                        hour: "כל הישיבה"
+                    },
+                ],
+                [
+                    // no class here
+                ],
+                [
+                    {
+                        topic: "שבת שלום",
+                        schoolGrade: " ",
+                        name: " ",
+                        hour: " "
+                    }
+                ]
+                ]
+        }
+    ]
+    // Get the data for the specified day
+    const dayData = data[dayIndex - 1];
+
+    // Get the morning, noon, and evening containers
+    const morningContainer = document.querySelector("[morning]");
+    const noonContainer = document.querySelector("[noon]");
+    const eveningContainer = document.querySelector("[evening]");
+
+    // Clear the existing content of the containers
+    morningContainer.innerHTML = "";
+    noonContainer.innerHTML = "";
+    eveningContainer.innerHTML = "";
+
+    // Get the learning unit template
+    const learningUnitTemplate = document.querySelector("[learning-unit-template]");
+
+    // Loop through the parts of the day
+    for (const [partIndex, partData] of dayData.parts.entries()) {
+        // Get the container for the current part
+        let partContainer;
+        if (partIndex === 0) {
+            partContainer = morningContainer;
+        } else if (partIndex === 1) {
+            partContainer = noonContainer;
+        } else {
+            partContainer = eveningContainer;
+        }
+
+        // Loop through the units in the current part
+        for (const unitData of partData) {
+            // Create a new learning unit element
+            const learningUnit = learningUnitTemplate.content.cloneNode(true);
+
+            // Update the content of the learning unit
+            learningUnit.querySelector("[topic]").textContent = unitData.topic;
+            learningUnit.querySelector("[audience]").textContent = unitData.schoolGrade;
+            learningUnit.querySelector("[teacher]").textContent = unitData.name;
+            learningUnit.querySelector("[learnTime]").textContent = unitData.hour;
+
+            // Append the learning unit to the part container
+            partContainer.appendChild(learningUnit);
+        }
+    }
+}
+loadCalendarDay(1);
